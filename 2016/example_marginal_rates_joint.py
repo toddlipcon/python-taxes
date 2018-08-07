2b0dee57b088d90d63069406efc0302d2471a9a0
Todd Lipcon <todd@cloudera.com>
2018-08-06 22:22:11 -0700

    more random hacking

--- 2016/example_marginal_rates_joint.py
+++ 2016/example_marginal_rates_joint.py
@@ -31,7 +31,12 @@ template = {
 SOCIAL_SECURITY_MAX = 117000
 MEDICARE_RATE = .0145
 
-def compute_with_income(template, incomes=[0,0], capital_gains=0, re_tax=0, amt_basis_adj=0):
+def compute_with_income(template,
+        incomes=[0,0],
+        capital_gains=0,
+        re_tax=0,
+        mortgage_interest=0,
+        amt_basis_adj=0):
     inputs = copy.deepcopy(template)
     inputs['real_estate_taxes'] = re_tax
     inputs['wages']          = incomes
@@ -43,6 +48,9 @@ def compute_with_income(template, incomes=[0,0], capital_gains=0, re_tax=0, amt_
     inputs['state_withholding'] = (sum(incomes) + capital_gains) * .10
     inputs['capital_gain_long'] = capital_gains
     inputs['amt_basis_adjustment'] = amt_basis_adj
+    inputs['F1040sa'] =  {
+                         '10' : mortgage_interest, # mortgage interest deduction
+                         }
     return F1040(inputs)
 
 DELTA = 10
@@ -59,39 +67,34 @@ def calc_rates(income,
                re_tax=0,
                ltcg=0,
                amt_basis_adj = 0,
+               mortgage_interest = 0,
                rate='MARGINAL',
                spouse_ratio=0.5):
     spouse_income = calc_spouse_split(income, ratio=spouse_ratio)
-    fbase = compute_with_income(template,
-                                incomes=spouse_income,
-                                capital_gains=(ltcg),
-                                amt_basis_adj=amt_basis_adj,
-                                re_tax=re_tax)
+    base_args = dict(incomes=spouse_income,
+                     capital_gains=ltcg,
+                     amt_basis_adj=amt_basis_adj,
+                     mortgage_interest=mortgage_interest,
+                     re_tax=re_tax)
+    fbase = compute_with_income(template, **base_args)
+    next_args = base_args.copy()
     if rate == 'LTCG_MARGINAL':
-        fnext = compute_with_income(template,
-                                       incomes=spouse_income,
-                                       capital_gains=(ltcg + DELTA),
-                                       amt_basis_adj=amt_basis_adj,
-                                       re_tax=re_tax)
-        capgain_rate = float(fnext['63'] - fbase['63']) / DELTA
-        return capgain_rate
+        next_args['capital_gains'] += DELTA
+        fnext = compute_with_income(template, **next_args)
+        return float(fnext['63'] - fbase['63']) / DELTA
+    elif rate == 'INT_DEDUCTION_MARGINAL':
+        next_args['mortgage_interest'] += DELTA
+        fnext = compute_with_income(template, **next_args)
+        return float(fnext['63'] - fbase['63']) / DELTA
     elif rate == 'RE_DEDUCTION_MARGINAL':
-        fnext = compute_with_income(template,
-                                       incomes=spouse_income,
-                                       capital_gains=ltcg,
-                                       amt_basis_adj=amt_basis_adj,
-                                       re_tax=(re_tax+DELTA))
-        capgain_rate = float(fnext['63'] - fbase['63']) / DELTA
-        return capgain_rate
+        next_args['re_tax'] += DELTA
+        fnext = compute_with_income(template, **next_args)
+        return float(fnext['63'] - fbase['63']) / DELTA
     elif rate == 'MARGINAL':
-        spouse_income_delta = calc_spouse_split(income + DELTA, ratio=spouse_ratio)
-        fnext = compute_with_income(template,
-                                    incomes=spouse_income_delta,
-                                    capital_gains=ltcg,
-                                    amt_basis_adj=amt_basis_adj,
-                                    re_tax=re_tax)
-        rate = float(fnext['63'] - fbase['63']) / DELTA
-        return rate
+        next_args['incomes'] = calc_spouse_split(income + DELTA, ratio=spouse_ratio)
+        fnext = compute_with_income(template, **next_args)
+        fnext = compute_with_income(template, **next_args)
+        return float(fnext['63'] - fbase['63']) / DELTA
     elif rate == 'EFFECTIVE':
         return fbase['63'] / (income + ltcg)
     elif rate == 'TOTAL':
@@ -103,6 +106,7 @@ MIN_LTCG = 0
 MAX_LTCG = 1000000
 MIN_WAGES = 10000
 MAX_WAGES = 1000000
+MAX_MORTGAGE_INTEREST = 100000
 WAGES_STEP = 10000
 incomes = np.arange(MIN_WAGES, MAX_WAGES, WAGES_STEP)
 
@@ -114,6 +118,7 @@ plt.legend(loc=4)
 p_ltcg, = plt.plot(incomes, incomes, label="LTCG Marginal")
 p_wages, = plt.plot(incomes, incomes, label="W2 Marginal")
 p_deduct, = plt.plot(incomes, incomes, label="Local tax marginal Deduction")
+p_int_deduct, = plt.plot(incomes, incomes, label="Mortgage marginal Deduction")
 plt.grid(True)
 ax_marginal.set_ylim([-0.6, 0.60])
 ax_marginal.legend()
@@ -135,19 +140,25 @@ ltcg_slider = Slider(ax_ltcg, 'LTCG', MIN_LTCG, MAX_LTCG, valinit=0, valfmt="%d"
 ax_amt_basis_adj = plt.axes([0.15, 0.15, 0.75, 0.03], facecolor="lightgoldenrodyellow")
 amt_basis_adj_slider = Slider(ax_amt_basis_adj, 'AMT Basis Adj', -MAX_LTCG, MAX_LTCG, valinit=0, valfmt="%d")
 
+ax_mortgage_interest = plt.axes([0.15, 0.2, 0.75, 0.03], facecolor="lightgoldenrodyellow")
+mortgage_interest_slider = Slider(ax_mortgage_interest, 'Mtg int', 0, MAX_MORTGAGE_INTEREST, valinit=0, valfmt="%d")
+
 def update(unused=None):
     inputs = dict(
         re_tax = re_tax_slider.val,
+        mortgage_interest = mortgage_interest_slider.val,
         amt_basis_adj = amt_basis_adj_slider.val,
         ltcg = ltcg_slider.val)
     p_ltcg.set_ydata(calc_rates(incomes, rate='LTCG_MARGINAL', **inputs))
     p_wages.set_ydata(calc_rates(incomes, rate='MARGINAL', **inputs))
     p_effective.set_ydata(calc_rates(incomes, rate='EFFECTIVE', **inputs))
     p_deduct.set_ydata(calc_rates(incomes, rate='RE_DEDUCTION_MARGINAL', **inputs))
+    p_int_deduct.set_ydata(calc_rates(incomes, rate='INT_DEDUCTION_MARGINAL', **inputs))
     fig.canvas.draw_idle()
 
 re_tax_slider.on_changed(update)
 ltcg_slider.on_changed(update)
 amt_basis_adj_slider.on_changed(update)
+mortgage_interest_slider.on_changed(update)
 update()
 plt.show()
